a45a88d7e5da77e48160b7389f65d436e8b39f5a
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-23 22:29:52 +0100

    Complete App.js
    States: loading, jobs, active
    Load data once when the page loads
    handleClick on .job-btn
    etc.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,70 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [active, setActive] = useState({});
+
+  const handleClick = (e) => {
+    const [displayedJob] = jobs.filter(job => job.company === e.target.textContent);
+    setActive(displayedJob);
+  }
+
+  useEffect(function() {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setJobs(data);
+        setActive(data[0]);
+        setLoading(false);
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+  if (loading) {
+    return (
+      <main className="section title">
+        <h1>Loading...</h1>
+      </main>
+    )
+  }
+  
+  return (
+    <main className="section">
+      <header className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </header>
+      <div className="jobs-center">
+        <nav className="btn-container">
+        {jobs.map(job => {
+          return (
+            <button
+              className={`job-btn${active.title === job.title ? " active-btn" : ""}`}
+              onClick={handleClick}
+              key={job.id}>{job.company}</button>
+          )
+        })}
+        </nav>
+        <article className="job-info">
+          <h3>{active.title}</h3>
+          <h4>{active.company}</h4>
+          <p className="job-date">{active.dates}</p>
+          <ul>
+            {active.duties.map(duty => {
+              return (
+                <li className="job-desc" key={duty}>
+                  <i className="job-icon"><FaAngleDoubleRight /></i>
+                  <p>{duty}</p>
+                </li>
+              )
+            })}
+          </ul>
+        </article>
+      </div>
+      <button className="btn">more info</button>
+    </main>
+  )
 }
 
 export default App
