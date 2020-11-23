6e804cbe2c0ce257f2201834c71a3e91b3dddf87
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-23 22:46:48 +0100

    Updates

---
+++ precache-manifest.1fb4e304c6d9259618054015c62776dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ddc4f34f9f58daad2d35b4186ca7084",
+    "url": "/experience-tabs/index.html"
+  },
+  {
+    "revision": "216b00a109db89e89988",
+    "url": "/experience-tabs/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "4be8803e14da57380620",
+    "url": "/experience-tabs/static/js/2.b7b4eab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/experience-tabs/static/js/2.b7b4eab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216b00a109db89e89988",
+    "url": "/experience-tabs/static/js/main.2726c8d8.chunk.js"
+  },
+  {
+    "revision": "d725203c7aec64d29b74",
+    "url": "/experience-tabs/static/js/runtime-main.cf3eca6d.js"
+  }
+]);
